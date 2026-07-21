efa6d6bfded0d86d2ebc673726ad69fd9f9dc5b9
Mateus Brum <mateushenriquebrum@gmail.com>
2026-07-21 22:11:23 +0100

    Basic DSL created

--- src/main/java/com.facta/Facta.java
+++ src/main/java/com.facta/Facta.java
@@ -1,19 +1,35 @@
 package com.facta;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.function.Function;
+
+import static java.util.List.of;
 
 public class Facta<B> {
-    public static class Seq<B> {
-        public Live<B> With(B board) {
-            Root.Context<B> context = new Root.Context<>(board, new HashMap<>(), new HashSet<>());
-            Node<B> sequence = new Node.Sequence<>(List.of());
-            return new Live<B>(sequence, context);
-        }
+
+    private Facta() {}
+    private static int index = 0;
+
+    @SafeVarargs
+    public static <B> Node.Sequence<B> Sequence(Node<B> ... children) {
+        return new Node.Sequence<>(of(children));
     }
 
-    public static <B> Seq<B> Sequence() {
-        return new Seq<>();
+
+    @SafeVarargs
+    public static <B> Node.Fallback<B> Fallback(Node<B> ... children) {
+        return new Node.Fallback<>(of(children));
     }
+
+    public static <B> Node.Belief<B> Belief(Function<B, Boolean> condition) {
+        return new Node.Belief<>(condition);
+    }
+
+    public static <B> Node.Action<B> Action(Function<B, Node.Status> perform) {
+        return new Node.Action<>(index++, perform);
+    }
+
+    public static <B> Node.Inverse<B> Inverse(Node.Belief<B> belief) {
+        return new Node.Inverse<>(belief);
+    }
+
 }
--- src/test/java/com/facta/FactTest.java
+++ src/test/java/com/facta/FactTest.java
@@ -3,14 +3,16 @@ package com.facta;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.function.Function;
 
-import static com.facta.Facta.Sequence;
+import static com.facta.Facta.*;
+import static java.lang.Boolean.TRUE;
+import static java.util.List.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactTest {
     
-    class Board {
+    static class Board {
         boolean home = false;
         int x, y = 0;
         
@@ -24,10 +26,37 @@ public class FactTest {
     }
     
     @Test
-    public void shouldSequence() {
+    public void shouldExpressEmptySequence() {
         assertEquals(
-                new Node.Sequence<Board>(List.of()),
-                Sequence().With(board).node);
+                new Node.Sequence<Board>(of()),
+                Sequence());
+    }
 
+    @Test
+    public void shouldExpressEmptyFallback() {
+        assertEquals(
+                new Node.Fallback<Board>(of()),
+                Fallback());
+    }
+
+    @Test
+    public void shouldExpressBelief() {
+        Function<Board, Boolean> lambda = (board) -> TRUE;
+        assertEquals(
+                new Node.Fallback<>(of(new Node.Inverse<>(new Node.Belief<>(lambda)))),
+                Fallback(Inverse(Belief(lambda))));
+    }
+
+    @Test
+    public void shouldExpressActionWithProperIds() {
+        Function<Board, Node.Status> lambda = (board) -> Node.Status.SUCCESS;
+        assertEquals(
+                new Node.Fallback<>(of(
+                        new Node.Action<>(0, lambda),
+                        new Node.Action<>(1, lambda)
+                )),
+                Fallback(
+                        Action(lambda),
+                        Action(lambda)));
     }
 }
