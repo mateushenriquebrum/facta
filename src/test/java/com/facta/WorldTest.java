6d3d2f8ca2926152cc90277ed322175d8bb98604
Mateus Brum <mateushenriquebrum@gmail.com>
2026-07-23 21:01:01 +0100

    Improve test coverage

--- src/test/java/com/facta/WorldTest.java
+++ src/test/java/com/facta/WorldTest.java
@@ -48,6 +48,24 @@ public class WorldTest {
         )), world.run(1));
     }
 
+    @Test
+    public void shouldBeliefFailSafely() {
+        DummyBoard board = new DummyBoard();
+        Map<Integer, Function<DummyBoard, Boolean>> believes = new HashMap<>();
+        believes.put(1, (b) -> {
+            throw new RuntimeException();
+        });
+        Map<Integer, Function<DummyBoard, Boolean>> actions = new HashMap<>();
+        Node root = new Sequence(0,
+                new Belief(1));
+
+        World<?> world = new World<>(board, believes, actions, root);
+
+        assertEquals(new Ticked(List.of(
+                new StateOf(1, FAILURE)
+        )), world.run(2));
+    }
+
     static class DummyBoard {}
 
 }
